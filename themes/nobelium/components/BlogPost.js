75b28d0073aff9b62d85c515d7486c270b8e5cc6
kikohz <kikohz@gmail.com>
2023-01-02 11:35:03 +0800

    修改首页列表样式

--- themes/nobelium/components/BlogPost.js
+++ themes/nobelium/components/BlogPost.js
@@ -3,6 +3,7 @@ import BLOG from '@/blog.config'
 import formatDate from '@/lib/formatDate'
 
 const BlogPost = ({ post }) => {
+  console.log(post)
   return (
     <Link href={`${BLOG.SUB_PATH}/${post.slug}`}>
       <a>
