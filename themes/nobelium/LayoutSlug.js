c5bff5fab8d69609a6fd3f98cb128e1c33eb5e9a
kikohz <kikohz@gmail.com>
2023-01-02 14:23:26 +0800

    修改正文页面使用page_cover 图片

--- themes/nobelium/LayoutSlug.js
+++ themes/nobelium/LayoutSlug.js
@@ -6,10 +6,13 @@ import Comment from '@/components/Comment'
 import { ArticleFooter } from './components/ArticleFooter'
 
 export const LayoutSlug = props => {
-  const { post, lock, validPassword } = props
-
+  const { post, lock, validPassword ,page_cover} = props
   if (!post) {
     return <LayoutBase {...props} />
+  }
+  let defaultCover = 'https://objectstorage.ap-seoul-1.oraclecloud.com/n/cno3iavztv8w/b/mybox/o/blog-default.jpg'
+  if(post.page_cover) {
+    defaultCover = post.page_cover
   }
 
   return (
@@ -20,6 +23,7 @@ export const LayoutSlug = props => {
                 {!lock && <div id="notion-article" className="px-2">
                     {post && <>
                         <ArticleInfo post={post} />
+                        <img className="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 h-48 w-full object-cover shadow opacity-85" src={defaultCover}></img>
                         <NotionPage post={post} />
                         <Comment frontMatter={post}/>
                         <ArticleFooter />
